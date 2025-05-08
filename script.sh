5fafcb61718371553ecbff97c440a2fd14e7e00d
Kalyangali12 <galikalyan12@gmail.com>
2025-05-08 15:17:39 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Updating the System"
+sudo apt update -y
+
+echo "Installing Utilities"
+sudo apt install zip unzip -y 
+
+echo "Installing NGINX Web Server"
+sudo apt install nginx -y
+
+echo "Cleanup Document Root"
+sudo rm -rf /var/www/html
+
+echo "Clone Login App"
+sudo git clone https://github.com/kalyangali12/food.git /var/www/html
+
+echo "Finished Deployment Process"
